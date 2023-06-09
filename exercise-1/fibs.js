46e562588cc5acb87129c475f8cbd04ae501f961
Sophie Little <119007056+erinsophie@users.noreply.github.com>
2023-06-09 20:14:22 +0100

    Fix linting

--- exercise-1/fibs.js
+++ exercise-1/fibs.js
@@ -1,16 +1,16 @@
 function fibs(n) {
-    let arr = [0, 1]
-    
-    if(n === 0) return 'Invalid input'
-    if(n === 1) return [0]
+  let arr = [0, 1];
 
-    for(let i = 2; i < n; i++) {
-        arr.push(arr[i - 2] + arr[i - 1])
-    }
-  
-    return arr
+  if (n === 0) return 'Invalid input';
+  if (n === 1) return [0];
+
+  for (let i = 2; i < n; i++) {
+    arr.push(arr[i - 2] + arr[i - 1]);
+  }
+
+  return arr;
 }
 
-console.log(fibs(1)) // [ 0 ]
-console.log(fibs(2)) // [ 0, 1 ]
-console.log(fibs(8)) // [ 0, 1, 1, 2, 3, 5, 8, 13 ]
+console.log(fibs(1)); // [ 0 ]
+console.log(fibs(2)); // [ 0, 1 ]
+console.log(fibs(8)); // [ 0, 1, 1, 2, 3, 5, 8, 13 ]
--- exercise-1/fibsRec.js
+++ exercise-1/fibsRec.js
@@ -1,12 +1,12 @@
 function fibsRec(n) {
-    if(n === 0) return 'Inavlid input'
-    if(n === 1) return [0]
-    if(n === 2) return [0, 1]
-    
-     let arr = fibsRec(n - 1)
-     return arr.concat(arr[arr.length - 1] + arr[arr.length - 2])
+  if (n === 0) return 'Inavlid input';
+  if (n === 1) return [0];
+  if (n === 2) return [0, 1];
+
+  const arr = fibsRec(n - 1);
+  return arr.concat(arr[arr.length - 1] + arr[arr.length - 2]);
 }
 
-console.log(fibsRec(1)) // [ 0 ]
-console.log(fibsRec(2)) // [ 0, 1 ]
-console.log(fibsRec(8)) // [ 0, 1, 1, 2, 3, 5, 8, 13 ]
+console.log(fibsRec(1)); // [ 0 ]
+console.log(fibsRec(2)); // [ 0, 1 ]
+console.log(fibsRec(8)); // [ 0, 1, 1, 2, 3, 5, 8, 13 ]
